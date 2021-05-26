ddbddfb6d86a3199acb38aed7f5957ad315eeb1d
asgaulti <asgaulti@student.42.fr>
2021-05-26 17:57:34 +0200

    ft_push1

--- srcs/ft_push.c
+++ srcs/ft_push.c
@@ -6,7 +6,7 @@
 /*   By: asgaulti@student.42.fr <asgaulti>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 14:32:43 by asgaulti@st       #+#    #+#             */
-/*   Updated: 2021/05/26 16:13:11 by asgaulti@st      ###   ########.fr       */
+/*   Updated: 2021/05/26 17:53:49 by asgaulti@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ void	ft_push_a(t_stack **a, t_stack **b)
 {
 	t_stack *tmp;
 	
-	if (!b)
+	if (!b || !a)
 		return ;
 	tmp = *a;
-	*a = *b;
-	a->next = tmp;
+	*a = *b; // faire pter b sur le 1er el de a
+	*b = (*b)->next; // dans b, faire demarrer la liste sur l'el suivant celui qu'on a retire
+	(*a)->next = tmp; // le 1er el de a (le 1er de b qu'on a mis comme debut) pointe sur l'ancien 1erel de a qu'on avait stocke dans tmp
 	printf("pa\n");
 }
 
@@ -28,10 +29,11 @@ void ft_push_b(t_stack **a, t_stack **b)
 {
 	t_stack *tmp;
 	
-    if (!a)
+    if (!a || !b)
         return ;
 	tmp = *b;
 	*b = *a;
-	b->next = tmp;
+	*a = (*a)->next;
+	(*b)->next = tmp;
     printf("pb\n");
 }
